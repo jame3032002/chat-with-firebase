6c4771f9b00c035f7f95603e1f6fafdfd6ae8299
jEsMaMoM <jame3032002@hotmail.com>
2019-06-30 21:13:20 +0700

    :cat: [ADD] เพิ่ม Component EmptyNotification

---
+++ components/Navbar/Notification/EmptyNotification.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Comments } from 'styled-icons/fa-solid'
+
+const ContainerEmptyNotification = styled.span`
+  font-family: Sarabun;
+  font-size: 1em;
+`
+
+const IconComments = styled(Comments)`
+  width: 24px;
+  height: 24px;
+  margin-right: 15px;
+  color: rgba(20, 27, 38, 0.20);
+`
+
+/** =============================================
+ * @name EmptyNotification
+ * @description แสดงกล่องว่า ไม่มีข้อมูล Notification
+ * label - คือคำที่แสดงกับ Icon Comments
+ * @version
+ * =========================================== */
+const EmptyNotification = ({ label }) => {
+  return (
+    <ContainerEmptyNotification>
+      <IconComments />
+      { label }
+    </ContainerEmptyNotification>
+  )
+}
+
+export default EmptyNotification
